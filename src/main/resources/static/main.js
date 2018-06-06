972b2e19db5f75875f7ab77d6af81b0e230dd02a
Saurabh Totey <SaurabhTotey@gmail.com>
2018-06-05 21:01:58 -0600

    Started making JavaScript to make the API calls from the web page

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,26 @@
+/**
+ * Handles text to morse conversion preview
+ */
+var previewInput = document.getElementById('text-preview-input');
+var previewOutput = document.getElementById('text-preview-output');
+previewInput.oninput = function () {
+    fetch('/pi-morse?message=' + encodeURI(previewInput.value)).then(function (response) {
+        response.text().then(function (body) {
+            previewOutput.value = body;
+        });
+    });
+};
+
+/**
+ * Handles submitting text to Raspberry Pi LED
+ */
+var submissionInput = document.getElementById('text-submission-input');
+var submissionOutput = document.getElementById('text-submission-output');
+var submissionButton = document.getElementById('text-submission-button');
+submissionButton.onclick = function () {
+    fetch('/pi-morse?message=' + encodeURI(submissionInput.value), { method: 'POST' }).then(function (response) {
+        response.json().then(function (result) {
+            submissionOutput.value = result.details;
+        });
+    });
+};
